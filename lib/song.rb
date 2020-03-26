57786f6a8063dc493bbc66dcdbb8d737c25214cb
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-26 19:59:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,13 +32,14 @@ class Song
     genre_count
   end
   def self.artist_count 
-    artist_count = {}
+   artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist] += 1 
-      else 
-        artist_count[artist] = 1 
-      end 
-    end 
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
     artist_count
-  end 
+  end
 end 
