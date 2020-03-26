6f9907f2e4d52411f49e185485206c0575aa44c4
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-26 19:56:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,6 +24,8 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        
+        genre_count[genre] += 1 
+      else 
+        genre_count
   end
 end 
